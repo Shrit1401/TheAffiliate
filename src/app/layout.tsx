ef7ec3e2dd142a16c7233fe0b2be790229d478b4
Shrit1401 <shrit1401@email.com>
2024-05-31 20:27:27 +0530

    Serious SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,28 +7,25 @@ import Footer from "@/components/Global/Footer";
 
 import MetadataImage from "../../public/images/card.png";
 
+export const alt = "About TheAffiliate";
+export const size = {
+  width: "1500",
+  height: "700",
+};
+
+export const contentType = "image/png";
+
+export const title =
+  "TheAffiliate - Your Ultimate Affiliate Programs Directory";
+
+export const desc =
+  "Discover and compare the best affiliate programs on TheAffiliate. Upvote your favorites, find expert insights, and boost your affiliate marketing success today!";
+
 export const metadata: Metadata = {
-  title: "TheAffiliate - Your Ultimate Affiliate Programs Directory",
-  description:
-    "Discover and compare the best affiliate programs on TheAffiliate. Upvote your favorites, find expert insights, and boost your affiliate marketing success today!",
+  title: `${title}`,
+  description: `${desc}`,
   icons: {
     icon: "images/icon_dark.svg",
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@shrit1401",
-  },
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    images: [
-      {
-        url: MetadataImage.src,
-        width: 1500,
-        height: 750,
-        alt: "TheAffiliate",
-      },
-    ],
   },
 };
 
@@ -40,6 +37,18 @@ export default function RootLayout({
   return (
     <html lang="en" className="z-10">
       <head>
+        <meta property="og:image" content={MetadataImage.src} />
+        <meta property="og:image:type" content={contentType} />
+        <meta property="og:image:width" content={size.width} />
+        <meta property="og:image:height" content={size.height} />
+        <meta property="og:image:alt" content={alt} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={MetadataImage.src} />
+        <meta name="twitter:image:type" content={contentType} />
+        <meta name="twitter:image:width" content={size.width} />
+        <meta name="twitter:image:height" content={size.height} />
+        <meta property="twitter:image:alt" content={alt} />
         <link
           rel="icon"
           href="images/favicon.ico"
