70b2583a549c650afe504a4fd6217b97992ee217
Shrit1401 <shrit1401@email.com>
2024-05-31 14:58:37 +0530

    Testimonials bug fixed

--- src/components/Home/Testimonial.tsx
+++ src/components/Home/Testimonial.tsx
@@ -3,12 +3,13 @@ import React from "react";
 export const Testimonial1 = () => {
   return (
     <section className="relative isolate overflow-hidden px-6 py-8 sm:py-12 md:px-8 bg-base-100">
-      <div className="mx-auto max-w-2xl lg:max-w-4xl">
+      <div className="mx-auto max-w-2xl md:max-w-4xl">
         <figure>
           <blockquote className="text-center font-semibold text-[1.5rem] md:text-3xl leading-9 md:leading-10">
-            "This website is a treasure trove of information! It's like having a
-            personal encyclopedia at your fingertips. I highly recommend it to
-            anyone looking to expand their knowledge or stay informed."
+            &quot;This website is a treasure trove of information! It&apos;s
+            like having a personal encyclopedia at your fingertips. I highly
+            recommend it to anyone looking to expand their knowledge or stay
+            informed.&quot;
           </blockquote>
           <figcaption className="mt-4">
             <div className="mt-4 flex items-center justify-center space-x-3">
@@ -22,7 +23,9 @@ export const Testimonial1 = () => {
               >
                 <circle cx={1} cy={1} r={1} />
               </svg>
-              <div className="text-white/50 font-bold">Full Stack Web Dev</div>
+              <div className="text-white/50 font-bold">
+                &quot;Full Stack Web Dev&quot;
+              </div>
             </div>
           </figcaption>
         </figure>
@@ -37,8 +40,8 @@ export const Testimonial2 = () => {
       <div className="mx-auto max-w-2xl lg:max-w-4xl">
         <figure>
           <blockquote className="text-center font-semibold text-[1.5rem]  md:text-3xl leading-9 md:leading-10">
-            "Become a part of the Affiliate.io community today and take your
-            marketing to the next level. "
+            &quot;Become a part of the Affiliate.io community today and take
+            your marketing to the next level. &quot;
           </blockquote>
           <figcaption className="mt-4">
             <div className="mt-4 flex items-center justify-center space-x-3">
